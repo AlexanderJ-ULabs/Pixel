5751765cb385f9792c3527fc90ed35bd1e560a63
Alexander Johnston <ajohnston@ulabs.io>
2018-06-08 16:02:23 -0400

    NetworkBrowser and Serilog
    Calling the WinAPI with logging. Basic commit.

---
+++ GateKeeper.Pixel/NetworkBrowser.cs
@@ -0,0 +1,85 @@
+﻿#region header
+// GateKeeper.Pixel - NetworkBrowser.cs
+// 
+// Copyright Untethered Labs, Inc.  All rights reserved.
+// 
+// Created: 2018-06-08 3:43 PM
+#endregion
+
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace GateKeeper.Pixel
+{
+    public sealed class NetworkBrowser
+    {
+        [DllImport("Netapi32", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int NetServerEnum(
+            string serverName,
+            int dwLevel,
+            ref IntPtr pBuf,
+            int dwPrefMaxLen,
+            out int dwEntriesRead,
+            out int dwTotalEntries,
+            int dwServerType,
+            string domain,
+            out int dwResumeHandle
+            );
+
+        [DllImport("Netapi32", SetLastError = true)]
+        public static extern int NetApiBufferFree(IntPtr pBuf);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServerInfo100
+        {
+            internal int sv100_platform_id;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            internal string sv100_name;
+        }
+
+        public static ArrayList GetNetworkComputers()
+        {
+            ArrayList networkComputers = new ArrayList();
+            const int MAX_PREFERRED_LENGTH = -1;
+            int SV_TYPE_WORKSTATION = 1;
+            int SV_TYPE_SERVER = 2;
+            IntPtr buffer = IntPtr.Zero;
+            IntPtr tmpBuffer = IntPtr.Zero;
+            int entriesRead;
+            int totalEntries;
+            int resHandle;
+            int sizeofInfo = Marshal.SizeOf(typeof(ServerInfo100));
+
+
+            try
+            {
+                int ret = NetServerEnum(null, 100, ref buffer,
+                                        MAX_PREFERRED_LENGTH, out entriesRead, out totalEntries,
+                                        SV_TYPE_WORKSTATION | SV_TYPE_SERVER, null, out resHandle);
+                if (ret == 0)
+                {
+                    for (int i = 0; i < totalEntries; i++)
+                    {
+                        tmpBuffer = new IntPtr((long)buffer + (i * sizeofInfo));
+
+                        ServerInfo100 svrInfo = (ServerInfo100)
+                                                   Marshal.PtrToStructure(tmpBuffer,
+                                                                          typeof(ServerInfo100));
+                        networkComputers.Add(svrInfo.sv100_name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                NetApiBufferFree(buffer);
+            }
+            return networkComputers;
+        }
+    }
+}
--- GateKeeper.Pixel/Program.cs
+++ GateKeeper.Pixel/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 
+using Serilog;
+
 namespace GateKeeper.Pixel
 {
     class Program
     {
+
+        private static bool LoggingEnabled { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to GateKeeper Pixel!");
+            Initialize(args);
+        }
+
+        static void Initialize (string[] args)
+        {
+            foreach (string arg in args)
+                if (arg.Contains("-log"))
+                    LoggingEnabled = true;
+
+            ConfigureSerilog();
+        }
+
+        static void ConfigureSerilog()
+        {
+            var logConfiguration = new LoggerConfiguration()
+                .WriteTo.Console();
+            if (LoggingEnabled)
+                logConfiguration.WriteTo.RollingFile("log-.txt");
+
+            var computers = NetworkBrowser.GetNetworkComputers();
+        }
+
+        private static string NormalizePath(string str)
+        {
+            str = str.Replace('\\', '/');
+            if (str.EndsWith("/"))
+            {
+
+            }
+            return "hi";
         }
     }
 }
